668af78df658e8a69d73cd4f7b3b52bc888c0eb3
Yeonho Lee <yeonhlee@c8r4s6.42seoul.kr>
2021-06-11 20:53:31 +0900

    03 ex00 make

---
+++ 03/ex00/FragTrap.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yeonhlee <yeonhlee@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/11 20:48:22 by yeonhlee          #+#    #+#             */
+/*   Updated: 2021/06/11 20:51:14 by yeonhlee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRAGTRAP_HPP
+# define FRAGTRAP_HPP
+
+class FragTrap
+{
+	private:
+		
+	public:
+		FragTrap(/* args */);
+		~FragTrap();
+};
+
+#endif
