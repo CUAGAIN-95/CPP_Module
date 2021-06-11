a75e6eef08348b3bbcd3852362ba5679ca4de95c
Yeonho Lee <yeonhlee@c8r4s6.42seoul.kr>
2021-06-11 22:53:58 +0900

    03 ex00 ˜ing...

--- 03/ex00/FragTrap.hpp
+++ 03/ex00/FragTrap.hpp
@@ -6,20 +6,48 @@
 /*   By: yeonhlee <yeonhlee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/11 20:48:22 by yeonhlee          #+#    #+#             */
-/*   Updated: 2021/06/11 20:51:14 by yeonhlee         ###   ########.fr       */
+/*   Updated: 2021/06/11 22:52:22 by yeonhlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FRAGTRAP_HPP
 # define FRAGTRAP_HPP
 
+#include <iostream>
+
 class FragTrap
 {
 	private:
-		
+		std::string		name;
+		int				hitPoint = 100;
+		int				maxHitPoint = 100;
+		int				energyPoint = 100;
+		int				maxEnergyPoints = 100;
+		int				level = 1;
+		int				meleeAttackDamage = 30;
+		int				rangedAttackDamage = 20;
+		int				armorDamageReduction = 5;
+
 	public:
-		FragTrap(/* args */);
+		FragTrap();
+		FragTrap(std::string _name);
+		FragTrap(FragTrap const &src);
 		~FragTrap();
+		
+		FragTrap	&operator=(FragTrap const &op);
+
+		/* GETTER*/
+		const std::string		&getName(void) const;
+
+		/* SETTER*/
+		void	setName(const std::string &_name);
+
+		/* MEMBER FUNCTION */
+		void	rangedAttack(std::string const &target);
+		void	meleeAttack(std::string const &target);
+		void	takeDamage(unsigned int amount);
+		void	beRepaired(unsigned int amount);
+		
 };
 
 #endif
